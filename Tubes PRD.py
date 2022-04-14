2a26ab210fe137b243bc52141402d7f294d1bf79
Nicholas <69570302+Nicholas438@users.noreply.github.com>
2022-04-14 15:19:06 +0700

    Update Tubes PRD.py

--- Tubes PRD.py
+++ Tubes PRD.py
@@ -1,2 +1,6 @@
 # Tubes PRD
-print("Selamat datang di aplikasi Traveller. Silahkan memilih jadwal keberangkatan dan kepulangan Anda: ")
+print("Selamat datang di aplikasi Traveller. Berapa lama Anda akan menjelajahi Bandung?")
+hari = input()
+print("\n")
+print("Kami menyarankan agar setiap hari hanya diisi 8-10 jam aktivitas untuk mengantisipasi kejadian tak terduga dan mengakomodasi waktu perjalanan")
+print("Silahkan memilih kategori aktivitas kegiatan yang ingin dilakukan: ")
