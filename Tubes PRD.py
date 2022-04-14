1aca3dff23b0b2441af25398a466bf02f4b8642e
Nicholas <69570302+Nicholas438@users.noreply.github.com>
2022-04-14 15:46:12 +0700

    Update Tubes PRD.py

--- Tubes PRD.py
+++ Tubes PRD.py
@@ -1,4 +1,86 @@
 # Tubes PRD
+def count_row(file):
+    # Subprogram untuk menghitung jumlah baris dari file csv
+    
+    # Kamus Lokal
+    # rowcount: int
+
+    # Algoritma
+    # Inisialisasi rowcount menjadi 0
+    rowcount  = 0
+    # Iterasi menghitung count sepanjang file
+    for row in file:
+        rowcount+= 1
+    return rowcount
+
+def parsing_file2(nama_tempat,var1,var2,var3,var4):
+    # Subprogram untuk menginisialisasi file data.csv
+
+    file_data = open('data.csv','r')
+    Lines = file_data.readlines()
+    
+    count = 0
+    for line in Lines:
+        nama_tempat[count] = ""
+        i = 0
+        while i<len(line):
+            if line[i] !=";":
+                nama_tempat[count]+=line[i]
+                i+=1
+            else:
+                break
+        i+=1
+        var1[count] = ""
+        while i<len(line):
+            if line[i] !=";":
+                var1[count]+=line[i]
+                i+=1
+            else:
+                break
+        i+=1
+        var2[count] = ""
+        while i<len(line):
+            if line[i] !=";":
+                var2[count]+=line[i]
+                i+=1
+            else:
+                break
+        i+=1
+        var3[count] = ""
+        while i<len(line):
+            if line[i] !=";":
+                var3[count]+=line[i]
+                i+=1
+            else:
+                break
+        i+=1
+        var4[count] = ""
+        if count != (row_data-1):
+            while i<(len(line)-1):
+                if line[i] !=";":
+                    var4[count]+=line[i]
+                    i+=1
+                else:
+                    break
+        else:
+            while i<len(line):
+                if line[i] !=";":
+                    var4[count]+=line[i]
+                    i+=1
+                else:
+                    break            
+        count+=1
+    return nama_tempat,var1,var2,var3,var4
+
+
+row_data = count_row(file_data)
+nama_tempat = [0 for i in range(row_data)]
+kategori_tempat = [0 for i in range(row_data)]
+kategori2 = [0 for i in range(row_data)]
+range_budget = [0 for i in range(row_data)]
+deskripsi = [0 for i in range(row_riwayat)]
+parsing_file2(nama_tempat,kategori_tempat,kategori2,range_budget,deskripsi)
+
 print("Selamat datang di aplikasi Traveller. Berapa lama Anda akan menjelajahi Bandung?")
 hari = input()
 print("\n")
